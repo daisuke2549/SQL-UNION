cea4858a5b6870fa0197ad4915ca7a5520e4ab0b
Daisuke Sasaki <67216632+daisuke2549@users.noreply.github.com>
2022-01-24 22:36:22 +0000

    CREATE table and use UNION ALL

--- main.sql
+++ main.sql
@@ -1,6 +1,25 @@
 create table UNION1(
   id INT,
   stock INT
-)
+);
 
-INSERT  INTO UNION1 VALUE(id, stock) VALUE (2,25);
+INSERT INTO UNION1 VALUES (1,25);
+INSERT INTO UNION1 VALUES (2,45);
+
+create table UNION2(
+  id INT,
+  stock INT
+);
+
+
+INSERT INTO UNION2 VALUES (3,65);
+INSERT INTO UNION2 VALUES (2,45);
+INSERT INTO UNION2 VALUES (5,85);
+
+-- select id, stock from UNION1
+-- UNION ALL
+-- select id, stock from UNION2
+
+select id, stock from UNION1
+UNION
+select id, stock from UNION2
