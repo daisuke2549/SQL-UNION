49b39fb4478d3c84374752e836aac5ce53720852
Daisuke Sasaki <67216632+daisuke2549@users.noreply.github.com>
2022-02-25 05:45:39 +0000

    likeであいまい検索の動作確認

--- main.sql
+++ main.sql
@@ -13,7 +13,9 @@ INSERT INTO employees VALUES('2', 'C0001','鈴木');
 INSERT INTO employees VALUES('3', 'C0002','伊藤');
 INSERT INTO employees VALUES('4', 'C0003','江藤');
 
-CREATE TABLE sample1_departments (
+-- select * from employees where last_name like '佐%'
+  
+CREATE TABLE departments (
   department_no     char(5),    
   department_name varchar(255),  
   PRIMARY KEY (department_no),
@@ -21,15 +23,15 @@ CREATE TABLE sample1_departments (
 );
  
 -- テストデータ
-INSERT INTO sample1_departments VALUES('C0001', '営業部');
-INSERT INTO sample1_departments VALUES('C0002', '総務部');
-INSERT INTO sample1_departments VALUES('C0003', '法務部');
-INSERT INTO sample1_departments VALUES('C0004', '人事部');
+INSERT INTO departments VALUES('C0001', '営業部');
+INSERT INTO departments VALUES('C0002', '総務部');
+INSERT INTO departments VALUES('C0003', '法務部');
+INSERT INTO departments VALUES('C0004', '人事部');
 
 SELECT * FROM 
   employees a -- 従業員テーブル 別名a
 INNER JOIN
-  sample1_departments b --  部署テーブル 別名b
+  departments b --  部署テーブル 別名b
 ON
   a.department_no = b.department_no; -- a.部署番号 = b.部署番号
 
