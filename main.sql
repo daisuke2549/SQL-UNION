3a8a9181fc1ad4e338bb3e12b0b21010ba5c871c
Daisuke Sasaki <67216632+daisuke2549@users.noreply.github.com>
2022-02-26 09:00:37 +0000

    ASCとDESCでorder by 並び替え

--- main.sql
+++ main.sql
@@ -1,16 +1,20 @@
 CREATE table items(
-  id int ,
+  id int auto_increment,
   item char,
   price int,
   PRIMARY KEY (id)
 );
 
-INSERT into items  (id, item, price) values (2, 'apple', 300);
-INSERT into items  (id, item, price) values (7, 'banana', 800);
-  INSERT into items  (id, item, price) values (8, 'melon', 1800);
+INSERT into items  (id, item, price) values (1,'apple', 300);
+INSERT into items  (id, item, price) values (2,'banana', 800);
+INSERT into items  (id, item, price) values (3,'melon', 1800);
+INSERT into items  (id, item, price) values (4,'strawberry', 100);
+INSERT into items  (id, item, price) values (5,'orange', 650);
 
-update items set price = 700,id = 3 where id = 2;
+-- update items set price = 700,id = 3 where id = 2;
 
 delete from items where id = 3;
+
+ALTER table items ADD keyword char;
   
-select * from items;
+select * from items order by price DESC;
